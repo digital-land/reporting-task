4a0c98b5df4b005c8cac8afac55e5fc53ab53483
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2025-09-28 12:49:36 +0100

    correct env varable name

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ make all -j 8
 
 # if bucket provided sync to reporting folder of that bucket
 if [ ! -z "$COLLECTION_DATA_BUCKET" ]; then
-    aws s3 sync ./reporting/ s3://$BUCKET/reporting/ --acl public-read
+    aws s3 sync ./data/reporting/ s3://$COLLECTION_DATA_BUCKET/reporting/
 else
     echo "COLLECTION_DATA_BUCKET not provided, skipping sync to S3"
 fi
