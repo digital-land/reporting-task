7bd6d9642b41e2f4ae3b9cf47781d5a8c6a2cf37
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2025-09-28 12:47:08 +0100

    add s3 sync command if bucket is provided

--- run.sh
+++ run.sh
@@ -2,3 +2,10 @@
 set -e
 
 make all -j 8
+
+# if bucket provided sync to reporting folder of that bucket
+if [ ! -z "$COLLECTION_DATA_BUCKET" ]; then
+    aws s3 sync ./reporting/ s3://$BUCKET/reporting/ --acl public-read
+else
+    echo "COLLECTION_DATA_BUCKET not provided, skipping sync to S3"
+fi
